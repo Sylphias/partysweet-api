569ab70eea704fb99debc70bda5c9d2a27d4fb6e
Sylphias <sylpharyon@gmail.com>
2014-12-07 01:29:38 +0800

    Add post request

--- app/api/resources/v1/parties.rb
+++ app/api/resources/v1/parties.rb
@@ -7,6 +7,42 @@ module Resources
           get '/', jbuilder: 'v1/parties/index.jbuilder' do
             @parties = Party.all
         end
+        desc "Add a new party with all the information" 
+          post '/' do
+            party = params[:party]
+            party_invitation = params[:party][:party_invitation]
+            party_users = params[:party][:party_user]
+            party_items = params[:party][:party_items]
+            party_resources = params[:party][:party_resources]
+            p = Party.create(
+              title: party[:title],
+              description: party[:description],
+              date: party[:date],
+              time: party[:time],
+              location: party[:location],
+              price: party[:price]
+              )
+            party_users.each do |u|
+              user = p.users.create(
+                name: u[:name], 
+                number: u[:number]
+              )
+              inv = Invitation.where(user_id: user.id, party_id: p.id).first
+              inv.host = u[:host]
+              inv.save
+            end
+            party_items.each do |i|
+              item = p.items.create(
+                name: i[:name],
+                price: i[:price]
+                )
+              res = Resource.where(item_id: item.id, party_id: p.id).first
+              res.quantity = i[:quantity]
+              res.save
+            end
+
+          end
+
       end
 
     #class starts here 
