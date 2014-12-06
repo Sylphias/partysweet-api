f499e0bd79338446d471a29ee5fc9c904725f008
Satrughan <sunnydeveloperacc@gmail.com>
2014-12-07 01:53:29 +0800

    Fixed cors

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,14 @@ module PartysweetApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.middleware.use Rack::Cors do
+        allow do
+          origins '*'
+      # location of your API
+      resource '/api/*', :headers => :any, :methods => [:get, :post, :options, :put]
   end
 end
+end
+end
+
+
